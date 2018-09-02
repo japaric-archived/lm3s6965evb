a8ff7925194f71dae75bd16ac84414fc1ff78587
Hideki Sekine <sekineh@me.com>
2018-09-03 01:02:12 +0900

    remove feature; add unreachable crate.

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,13 @@ extern crate cortex_m;
 
 #[macro_use(entry, exception)]
 extern crate cortex_m_rt as rt;
-
-#[cfg(feature = "semihosting")]
 extern crate cortex_m_semihosting as semihosting;
-
 extern crate panic_abort;
+extern crate unreachable;
 
 use core::arch::arm;
-
-use rt::ExceptionFrame;
-
-#[cfg(feature = "semihosting")]
 use core::fmt::Write;
+use rt::ExceptionFrame;
 
 entry!(main);
 
@@ -27,16 +22,16 @@ fn main() -> ! {
     loop {
         unsafe { arm::__NOP() }
 
-        #[cfg(feature = "semihosting")]
-        unsafe {
-            let mut hstdout = semihosting::hio::hstdout().unwrap();
-            write!(hstdout, "x = {}\n", x);
+        // write something through semihosting interface
+        let mut hstdout = semihosting::hio::hstdout().unwrap();
+        write!(hstdout, "x = {}\n", x);
 
-            // system call to exit successfully
-            semihosting::syscall1(
-                semihosting::nr::REPORT_EXCEPTION,
-                semihosting::debug::Exception::ApplicationExit as usize,
-            );
+        // exit from qemu
+        semihosting::debug::exit(semihosting::debug::EXIT_SUCCESS);
+
+        // hint to the optimizer that any code path which calls this function is statically unreachable
+        unsafe {
+            unreachable::unreachable();
         }
     }
 }
